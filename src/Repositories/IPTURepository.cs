f0ab4cd0683905774bd85fc1f4d498c23459c0c2
Rodrigo Alexandre Ceschi <rodrigoalexandre.sc@gmail.com>
2021-03-23 21:13:03 -0300

    kafka

--- src/Repositories/IPTURepository.cs
+++ src/Repositories/IPTURepository.cs
@@ -18,7 +18,7 @@ namespace ModCidadao.Repositories {
             try
             {
                 var data = dbContext.IPTUs.Where(w => 
-                    ( !string.IsNullOrEmpty(impostoQuery.CPFouCNPJ) ? impostoQuery.CPFouCNPJ == w.CPFouCNPJ : true) 
+                    ( !string.IsNullOrEmpty(impostoQuery.CPFouCNPJ) ? impostoQuery.CPFouCNPJ == w.CPFOuCNPJ : true) 
                     && (!string.IsNullOrEmpty(impostoQuery.InscricaoImovel) ? impostoQuery.InscricaoImovel == w.InscricaoImovel : true)
                     && (impostoQuery.DataConsulta.HasValue ? impostoQuery.DataConsulta >= w.DataVencimento : true));
                     
